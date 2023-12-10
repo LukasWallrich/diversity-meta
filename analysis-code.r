68729bd9833ddc6fb9e0046acc4aa854543546e2
Lukas Wallrich <lukas.wallrich@gmail.com>
2023-12-10 15:09:01 +0000

    Basic meta and equivalence tests done

---
+++ analysis-code.r
@@ -0,0 +1,93 @@
+# Load libraries
+library(tidyverse)
+library(readxl)
+library(metafor)
+library(clubSandwich)
+library(mice)
+library(metaCART)
+
+# Import data
+effect_sizes <- read_excel("path_to_final_effect_sizes.xlsx")
+
+# Conduct statcheck on PDF files
+# Note: Replace 'dummy_statcheck_function' with actual statcheck function
+effect_sizes <- effect_sizes %>%
+  mutate(statcheck_issue = map_lgl(filename, ~dummy_statcheck_function(.x)))
+
+# Conduct GRIM and GRIMMER tests
+# Note: Replace 'dummy_grim_function' and 'dummy_grimmer_function' with actual functions
+effect_sizes <- effect_sizes %>%
+  mutate(grim_issue = map_lgl(some_column, ~dummy_grim_function(.x)),
+         grimmer_issue = map_lgl(some_column, ~dummy_grimmer_function(.x)))
+
+# Convert all effect sizes to Pearson's r
+# Note: Implement conversion based on the formulas provided in the document
+effect_sizes <- effect_sizes %>%
+  mutate(r = map_dbl(effect_size, ~convert_to_pearsons_r(.x)))
+
+# Calculate the standard error for Rs based on the sample size
+effect_sizes <- effect_sizes %>%
+  mutate(se_r = sqrt((1 - r^2) / (n - 2)))
+
+# Impute the diversity_reliability and the measurement_reliability
+# Note: Use a sampling method with replacement based on the scale length categories
+effect_sizes <- effect_sizes %>%
+  mutate(diversity_reliability = dummy_impute_reliability(diversity_scale_length),
+         measurement_reliability = dummy_impute_reliability(measurement_scale_length))
+
+# Implement the correction for reliability/attenuation
+# Note: Apply the formula specified in the document
+effect_sizes <- effect_sizes %>%
+  mutate(corrected_r = apply_correction_for_reliability(r, diversity_reliability, measurement_reliability))
+
+# Create main effect meta-analysis, split by diversity domain
+meta_analysis_results <- effect_sizes %>%
+  group_by(diversity_domain) %>%
+  summarise(meta_analysis = dummy_meta_analysis(corrected_r, se_r)) # Replace with actual meta-analysis function
+
+# Adjust standard errors using clubSandwich
+meta_analysis_results <- meta_analysis_results %>%
+  mutate(adjusted_se = clubSandwich::vcovCR(meta_analysis, type = "CR2"))
+
+# Conduct equivalence test with 0.1 as the SESOI
+# Note: Replace 'dummy_equivalence_test' with actual equivalence test function
+meta_analysis_results <- meta_analysis_results %>%
+  mutate(equivalence = dummy_equivalence_test(meta_analysis, sesoi = 0.1))
+
+# Create a forest plot with 3 facets/subplots for diversity domains
+# Note: Adjust the plotting code based on your specific requirements
+ggplot(meta_analysis_results, aes(x = diversity_domain, y = corrected_r, ymin = corrected_r - se_r, ymax = corrected_r + se_r)) +
+  geom_pointrange() +
+  geom_hline(yintercept = c(-0.1, 0.1), linetype = "dashed") +
+  facet_wrap(~diversity_domain)
+
+# Impute data on the moderators (using mice)
+imputed_data <- mice(effect_sizes, method = 'pmm', m = 5) # Adjust parameters as needed
+
+# Conduct meta-regression, adjust standard errors, and combine multiply imputed estimates
+# Note: Replace 'dummy_meta_regression' with actual meta-regression function
+meta_regression_results <- with(imputed_data, dummy_meta_regression(~ moderator + corrected_r))
+
+# Single imputation for metaCART (best case)
+best_case_imputation <- complete(imputed_data, action = 1) # Use the first imputation as the best case
+
+# Single imputation for metaCART (worst case)
+# Note: Implement a bootstrapping method for imputation
+worst_case_imputation <- effect_sizes %>%
+  mutate(across(everything(), ~dummy_bootstrap_imputation(.x))) # Replace with actual bootstrapping function
+
+# Run and visualise metaCART on both datasets
+# Note: Replace 'dummy_metaCART_function' with the actual metaCART function
+metaCART_best_case <- dummy_metaCART_function(best_case_imputation)
+metaCART_worst_case <- dummy_metaCART_function(worst_case_imputation)
+
+# Save datasets
+write.csv(best_case_imputation, "best_case_imputation.csv")
+write.csv(worst_case_imputation, "worst_case_imputation.csv")
+
+# Robustness checks
+# Note: Implement the checks as specified in the document
+
+# Run publication bias tests with bootstrapping
+# Note: Replace 'dummy_publication_bias_test' with actual publication bias test function
+publication_bias_results <- dummy_publication_bias_test(effect_sizes, n_bootstrap = 5000)
